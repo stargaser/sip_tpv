fix reverse SIP calculation

reverse.py
@@ -1,6 +1,14 @@
 
+from __future__ import print_function, division, absolute_import
 # This code determines reverse coefficients from the forward ones
 import numpy as np
+from numba import jit
+from functools import lru_cache
+#import pdb
+
+@lru_cache(maxsize=512)
+def raiseit(a, b):
+    return(np.power(np.array(a),b))
 
 # function to evaluate polynomials
 def evalpoly(u, v, adist, bdist):
@@ -12,23 +20,33 @@ def evalpoly(u, v, adist, bdist):
     # Calculate forward orders from size of distortion matrices
     aorder = np.shape(adist)[0] - 1
     border = np.shape(bdist)[0] - 1
+    myshape = u.shape
+    uu = u.flatten()
+    vv = v.flatten()
+    uprime = uu.astype('float64')
+    vprime = vv.astype('float64')
 
-    uprime = u.astype('float64')
-    vprime = v.astype('float64')
+    udict = {}
+    vdict = {}
+    for i in range(max(aorder,border)+1):
+        udict[i] = np.power(uu,i)
+        vdict[i] = np.power(vv,i)
     i = aorder
     while (i >= 0):
         j = aorder - i
         while (j >= 0):
-            uprime += adist[i][j]*(u**i)*(v**j)
+            uprime += adist[i][j]*udict[i]*vdict[j]
             j -= 1
         i -= 1
     i = border
     while (i >= 0):
         j = border - i
         while (j >= 0):
-            vprime += bdist[i][j]*(u**i)*(v**j)
+            vprime += bdist[i][j]*udict[i]*vdict[j]
             j -= 1
         i -= 1
+    uprime = uprime.reshape(myshape)
+    vprime = vprime.reshape(myshape)
     return (uprime, vprime)
 
 def fitreverse(aporder, bporder, adist, bdist, u, v):
@@ -43,17 +61,32 @@ def fitreverse(aporder, bporder, adist, bdist, u, v):
     bpdist = np.zeros((bporder+1,bporder+1),'float64')
 
     (uprime, vprime) = evalpoly(u,v,adist,bdist)
+    updict = {}
+    vpdict = {}
+    for i in range(max(aporder,bporder)+5):
+        updict[i] = np.power(uprime.flatten(),i)
+        vpdict[i] = np.power(vprime.flatten(),i)
     udiff = u - uprime
-    (uvec, umat) = reversesetup(udiff, aporder, uprime, vprime)
     vdiff = v - vprime
-    (vvec, vmat) = reversesetup(vdiff, bporder, uprime, vprime)
 
-    # Now at last, we are ready for inversion
-    uinv = np.linalg.inv(umat)
-    vinv = np.linalg.inv(vmat)
+    mylist1 = []
+    mylist2 = []
+    for i in range(aporder+1):
+        for j in range(0, aporder - i + 1):
+            mylist1.append(updict[i]*vpdict[j])
+    for i in range(bporder+1):
+        for j in range(0, bporder - i + 1):
+            mylist2.append(updict[i]*vpdict[j])
 
-    apcoeffs = np.dot(uinv, uvec)
-    bpcoeffs = np.dot(vinv, vvec)
+    A = np.array(mylist1).T
+    B = udiff.flatten()
+
+    apcoeffs, r, rank, s = np.linalg.lstsq(A, B)
+
+    A = np.array(mylist2).T
+    B = vdiff.flatten()
+
+    bpcoeffs, r, rank, s = np.linalg.lstsq(A, B)
 
     # Load reverse distortion matrices
     extractcoeffs(apcoeffs, aporder, apdist)
@@ -61,45 +94,14 @@ def fitreverse(aporder, bporder, adist, bdist, u, v):
 
     return(apdist, bpdist)
 
-def reversesetup(pdiff, order, x, y):
-    """ Given pixel difference array ,order,
-        and x & y coord arrays,
-        return elements in vector vec and matrix mat
-        to set up least-squares solution for reverse
-        polynomial coefficients """
-
-    # This number gives the array dimensions (number of coefficients)
-    idim = (order*(order+3))/2
-
-    # Create vector and matrix for calculations
-    vec = np.zeros((idim,), 'float64')
-    mat = np.zeros((idim, idim), 'float64')
-    sumpdiff = np.sum(pdiff, axis=None)
-
-    index = -1
-    for i in range(order+1):
-        for j in range(order-i+1):
-            if (index >= 0):
-                vec[index] = sumpdiff*np.sum(np.power(x,i)*np.power(y,j), axis=None)
-                l = -1
-                for xpow in range(order+1):
-                    for ypow in range(order-xpow+1):
-                        if (l >= 0):
-                            mat[l][index] = \
-                                    np.sum(np.power(x,i+xpow)*np.power(y,j+ypow),
-                                                  axis=None)
-                        l += 1
-            index += 1
-    return(vec, mat)
 
 def extractcoeffs(coeffs, order, dist):
     """ Given a compact vector of coefficients and the
         polynomial order, extract them into matrix dist """
-    index = -1
+    index = 0
     for i in range(order+1):
-        for j in range(order-i+1):
-            if (index >= 0):
-                dist[i][j] = coeffs[index]
+        for j in range(0,order-i+1):
+            dist[i][j] = coeffs[index]
             index += 1
 
 

sip_to_pv.py
@@ -37,6 +37,7 @@ from sympy import symbols, Matrix, poly
 import numpy as np
 import astropy.io.fits as pyfits
 import reverse
+import pdb
 
 
 def sym_tpvexprs():
@@ -314,7 +315,7 @@ def add_pv_keywords(header, sipx, sipy, pvrange, tpvx, tpvy, tpv=True):
         header['CTYPE2'] = header['CTYPE2'][:8]
     return
 
-def add_sip_keywords(header, tpvu, tpvv, sipu, sipv):
+def add_sip_keywords(header, tpvu, tpvv, sipu, sipv, add_reverse):
     """Calculate the PV keywords and add to the header
 
     Parameters:
@@ -343,6 +344,8 @@ def add_sip_keywords(header, tpvu, tpvv, sipu, sipv):
     header['CTYPE2'] = 'DEC--TAN-SIP'
     header['A_ORDER'] = int(a_order)
     header['B_ORDER'] = int(b_order)
+    if add_reverse:
+        add_reverse_coefficients(header, a_order + 1, b_order + 1)
     return
 
 
@@ -417,6 +420,7 @@ def remove_pv_keywords(header):
 
 
 def add_reverse_coefficients(header, aporder, bporder):
+    #pdb.set_trace()
     crpix1 = header['CRPIX1']
     crpix2 = header['CRPIX2']
     naxis1 = header['NAXIS1']
@@ -429,13 +433,13 @@ def add_reverse_coefficients(header, aporder, bporder):
     bdist = np.array(bc)
     apdist,bpdist = reverse.fitreverse(aporder,bporder,adist,bdist, u,v)
     for i in range(aporder+1):
-        for j in range(aporder+1):
-            if ((i + j) <= aporder) and ((i + j) >= 1):
+        for j in range(0, aporder - i + 1):
                 header['AP_%d_%d'%(i,j)] = apdist[i,j]
     for i in range(bporder+1):
-        for j in range(bporder+1):
-            if ((i + j) <= bporder) and ((i + j) >= 1):
+        for j in range(0, bporder - i + 1):
                 header['BP_%d_%d'%(i,j)] = bpdist[i,j]
+    header['AP_ORDER'] = aporder
+    header['BP_ORDER'] = bporder
     return
 
 
@@ -470,7 +474,7 @@ def sip_to_pv(infile,outfile,tpv_format=True,preserve=False,extension=0,overwrit
     else:
       return False
 
-def pv_to_sip(infile,outfile,preserve=False,extension=0,overwrite=True):
+def pv_to_sip(infile,outfile,preserve=False,add_reverse=True,extension=0,overwrite=True):
     """ Function which wraps the sip_to_pv conversion
 
     Parameters:
@@ -489,7 +493,7 @@ def pv_to_sip(infile,outfile,preserve=False,extension=0,overwrite=True):
     cd, pv1, pv2 = get_pv_keywords(header)
     sipu, sipv = sym_sipexprs()
     tpvu, tpvv = real_tpvexprs(cd, pv1, pv2)
-    add_sip_keywords(header, tpvu, tpvv, sipu, sipv)
+    add_sip_keywords(header, tpvu, tpvv, sipu, sipv, add_reverse)
     if (not preserve):
         remove_pv_keywords(header)
     hdu.writeto(outfile, overwrite=overwrite)