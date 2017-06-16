c31f18aa6a203dfbcab93c76f5b43b84ddcdaae3
Austin Beauchamp <austinbeauch@gmail.com>
2017-06-16 15:51:35 -0700

    Test round trip for pix2world and world2pix

--- sip_tpv/tests/test_transforms.py
+++ sip_tpv/tests/test_transforms.py
@@ -11,6 +11,10 @@ dir_name = os.path.split(__file__)[0]
 
 
 def test_sip2pv():
+    """
+    Test conversion of sip 2 pv keywords, ensure that both provide same ra/dec <--> x/y transforms.
+    """
+
     sip_header = fits.Header.fromtextfile(os.path.join(dir_name, 'data/IRAC_3.6um_sip.txt'))
     control_header = sip_header.copy()
     naxis1 = sip_header['NAXIS1']
@@ -27,11 +31,21 @@ def test_sip2pv():
 
     world1 = wsip.all_pix2world(pixargs, 1)
     world2 = wtpv.all_pix2world(pixargs, 1)
-    print("Test1")
+
     npt.assert_equal(world1, world2)
+
+    pix1 = wsip.all_world2pix(world1, 1)
+    pix2 = wtpv.all_world2pix(world2, 1)
+    npt.assert_almost_equal(pix1, pixargs, 4)
+    npt.assert_almost_equal(pix2, pixargs, 4)
 
 
 def test_pv2sip():
+    """
+    Test conversion of pv 2 sip keywords, check to see that world2pix transform round trips and is equal for pv and
+     sip keywords.
+    """
+
     pv_header = fits.Header.fromtextfile(os.path.join(dir_name, 'data/PTF_r_chip01_tpv.txt'))
     control_header = pv_header.copy()
     naxis1 = pv_header['NAXIS1']
@@ -48,11 +62,11 @@ def test_pv2sip():
 
     world1 = wsip.all_pix2world(pixargs, 1)
     world2 = wtpv.all_pix2world(pixargs, 1)
-    print("Test2")
+
     npt.assert_equal(world1, world2)
 
-    sip_to_pv(pv_header)
-    world1 = wsip.all_pix2world(pixargs, 1)
+    pix1 = wsip.all_world2pix(world1, 1)
+    pix2 = wtpv.all_world2pix(world2, 1)
 
-    print("Test3")
-    npt.assert_equal(world1, world2)
+    npt.assert_almost_equal(pix1, pixargs, 4)
+    npt.assert_almost_equal(pix2, pixargs, 4)
